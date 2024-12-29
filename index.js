359cae0a7f1bb0d23d66d7e34ee945321a600381
Andrew Kononenko <wastardy.k@gmail.com>
2024-12-29 17:16:47 +0200

    add test route in users.js and test it in index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import Express from "express";
+import userRoutes from "./routes/users.js";
+
 const app = Express();
+
+app.use('/api/users', userRoutes);
 
 app.listen(8800, () => {
     console.log('---> API is up to use');
