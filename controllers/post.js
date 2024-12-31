424d05566baf163e6441062daccc65e91a4bf332
Andrew Kononenko <wastardy.k@gmail.com>
2024-12-31 13:23:12 +0200

    filter posts in feed, add relationships between users

--- controllers/post.js
+++ controllers/post.js
@@ -1,14 +1,35 @@
 import { db } from "../connect.js";
-
+import jwt from 'jsonwebtoken';
 
 export const getPosts = (req, res) => {
-    const q = `SELECT p.*, u.id AS user_id, name, profile_picture FROM posts AS p JOIN users AS u ON (u.id = p.user_id)`;
+    const token = req.cookies.accessToken;
 
-    db.query(q, (err, data) => {
+    if (!token) {
+        return res.status(401).json('not logged in');
+    }
+
+    jwt.verify(token, 'secretkey', (err, userInfo) => {
         if (err) {
-            return res.status(500).json(err);
+            return res.status(403).json('token is not valid');
         }
 
-        return res.status(200).json(data);
+        const q = 
+            `SELECT p.*, u.id AS user_id, name, profile_picture 
+            FROM posts AS p 
+            JOIN users AS u 
+                ON (u.id = p.user_id)
+            LEFT JOIN relationships AS r 
+                ON p.user_id = r.followed_user_id
+            WHERE r.follower_user_id = ? 
+                OR p.user_id = ?
+            ORDER BY p.created_at DESC`;
+
+        db.query(q, [userInfo.id, userInfo.id], (err, data) => {
+            if (err) {
+                return res.status(500).json(err);
+            }
+
+            return res.status(200).json(data);
+        });
     });
 }
