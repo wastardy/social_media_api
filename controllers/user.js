99f7e59c2dc1e60c84b9befbf4d50aa0957fefe1
Andrew Kononenko <wastardy.k@gmail.com>
2025-01-01 20:38:50 +0200

    create an endpoint to update user profile data

--- controllers/user.js
+++ controllers/user.js
@@ -23,3 +23,44 @@ export const getUser = (req, res) => {
         return res.json(info);
     });
 }
+
+export const updateUser = (req, res) => {
+    const token = req.cookies.accessToken;
+    if (!token) {
+        return res.status(401).json("not authenticated!");
+    }
+
+    jwt.verify(token, "secretkey", (err, userInfo) => {
+        if (err) {
+            return res.status(403).json("token is not valid!");
+        }
+
+        const q = 
+            `UPDATE users
+            SET name = ?, 
+                city = ?, 
+                website = ?, 
+                cover_picture = ?, 
+                profile_picture = ?
+            WHERE id = ?`;
+        
+        db.query(q, [
+            req.body.name,
+            req.body.city,
+            req.body.website,
+            req.body.cover_picture,
+            req.body.profile_picture, 
+            userInfo.id
+        ], (error, data) => {
+            if (error) {
+                return res.status(500).json(err);
+            }
+
+            if (data.affectedRows > 0) {
+                return res.json('user profile info has been updated');
+            }
+
+            return res.status(403).json('you can update only your post');
+        });
+    })
+}
